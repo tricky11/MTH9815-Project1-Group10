cb5964ab9ff688fc72dac052575c88859adaaa23
Harish Reddy <harish.7000@gmail.com>
2017-11-08 00:02:48 -0500

    Monte carlo simulations for waterfall

--- waterfall.py
+++ waterfall.py
@@ -1,2 +1,8 @@
 def do_waterfall(loan_pool, structured_securities):
     pass
+
+
+def simulate_waterfall(loan_pool, structured_securities, nsim):
+    results = [do_waterfall(loan_pool, structured_securities) for i in range(nsim)]
+    sum_results = reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), results, (0, 0))
+    return sum_results[0] / nsim, sum_results[1] / nsim
